d6efac7da894f0b795cae37eb699aa7eff1872af
Ron Smits <ron.smits@gmail.com>
2013-01-01 00:19:14 +0100

    added an automatic id

--- src/main/java/com/example/model/Movie.java
+++ src/main/java/com/example/model/Movie.java
@@ -1,25 +1,39 @@
 package com.example.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @XmlRootElement
 public class Movie {
-	@Id
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
 	private String title;
 	private String watched;
 	private int scoring;
+	
+
+	public Movie() {
+		// TODO Auto-generated constructor stub
+	}
 	
 	public Movie (String title, String watched, int scoring) {
 		this.title = title;
 		this.watched = watched;
 		this.scoring = scoring;
 	}
-	public Movie() {
-		// TODO Auto-generated constructor stub
+	
+	public void setId(long id) {
+		this.id = id;
 	}
+	
+	public long getId() {
+		return id;
+	}
+	
 	public String getTitle() {
 		return title;
 	}
