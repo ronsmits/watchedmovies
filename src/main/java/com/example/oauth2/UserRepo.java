45ff98d2baf67330ca902fe5ca488af257958f14
Ron Smits <ron.smits@gmail.com>
2013-01-02 02:30:51 +0100

    and really checking in the added files

---
+++ src/main/java/com/example/oauth2/UserRepo.java
@@ -0,0 +1,34 @@
+package com.example.oauth2;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import com.example.model.User;
+
+@Stateless
+@LocalBean
+public class UserRepo {
+
+	@PersistenceContext private EntityManager manager;
+	
+	public void save(User user){
+		manager.merge(user);
+	}
+	
+	public User findUser(String username) {
+		TypedQuery<User> query = manager.createNamedQuery("user.findUser", User.class);
+		query.setParameter("username", username);
+		try {
+			User singleResult = query.getSingleResult();
+			return singleResult;
+		} catch (NoResultException nre){
+			// a new user!
+			System.out.println("exception: "+nre.getMessage());
+			return null;
+		}
+	}
+}
