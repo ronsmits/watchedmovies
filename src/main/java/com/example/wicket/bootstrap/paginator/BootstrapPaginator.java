c6ad37063736a70ab5591f5b7963ecffc2b61ba4
Ron Smits <ron.smits@gmail.com>
2013-01-11 00:04:13 +0100

    fixed the wrong commit

--- src/main/java/com/example/wicket/bootstrap/paginator/BootstrapPaginator.java
+++ src/main/java/com/example/wicket/bootstrap/paginator/BootstrapPaginator.java
@@ -4,6 +4,7 @@ import org.apache.wicket.markup.html.navigation.paging.IPageable;
 import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
 
 public class BootstrapPaginator extends PagingNavigator {
+	private static final long serialVersionUID = -6601654528238658380L;
 
 	public BootstrapPaginator(String id, IPageable pageable) {
 		super(id, pageable);
