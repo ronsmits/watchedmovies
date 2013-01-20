4e9b02087bbb67635cc4a33bda7f1b290cee2953
Ron Smits <ron.smits@gmail.com>
2013-01-20 23:15:33 +0100

    Logout works

--- src/main/java/com/example/wicket/panels/LoggedInPanel.java
+++ src/main/java/com/example/wicket/panels/LoggedInPanel.java
@@ -1,8 +1,15 @@
 package com.example.wicket.panels;
 
+import javax.servlet.http.Cookie;
+
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.StatelessForm;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.request.http.WebRequest;
+import org.apache.wicket.request.http.WebResponse;
+import org.apache.wicket.util.cookies.CookieUtils;
 
 import com.example.utils.StaticImage;
 
@@ -13,6 +20,20 @@ public class LoggedInPanel extends Panel {
 		super(id);
 		add(new Label("name", Model.of(name)));
 		add(new StaticImage("photo", new Model<String>(pictureURL)));
+		Form<Void> form = new StatelessForm<Void>("logoutform"){
+			private static final long serialVersionUID = 513129937918256386L;
+
+			@Override
+			public void onSubmit() {
+				System.out.println("called to delete");
+				WebRequest request = (WebRequest) getRequestCycle().getRequest();
+				Cookie cookie = request.getCookie("id");
+				WebResponse response = (WebResponse) getRequestCycle()
+						.getResponse();
+				response.clearCookie(cookie);
+			}
+		};
+		add(form);
 	}
 
 }
