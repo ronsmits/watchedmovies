dfdbbb7fbed3bd8b0f9f8db8b65e7d2882bafa2e
Ron Smits <ron.smits@gmail.com>
2014-03-02 20:50:51 +0100

    small stuff

--- src/main/java/org/ronsmits/watchedmovies/ejb/FilmRepo.java
+++ src/main/java/org/ronsmits/watchedmovies/ejb/FilmRepo.java
@@ -1,39 +1,39 @@
 package org.ronsmits.watchedmovies.ejb;
 
 import java.util.List;
-
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-
 import org.ronsmits.watchedmovies.model.Film;
 
 /**
- * Session Bean 
+ * Session Bean
  */
 @Stateless
 @LocalBean
 public class FilmRepo {
-	
-	@PersistenceContext private EntityManager manager;
+
+    @PersistenceContext
+    private EntityManager manager;
+
     /**
-     * Default constructor. 
+     * Default constructor.
      */
     public FilmRepo() {
-        // TODO Auto-generated constructor stub
+	// TODO Auto-generated constructor stub
     }
-    
+
     public List<Film> getList() {
-    	Query query = manager.createQuery("select m from Film m order by m.watched desc");
-    	@SuppressWarnings("unchecked")
-		List<Film> resultList = query.getResultList();
-    	return resultList;
+	Query query = manager.createQuery("select m from Film m order by m.title asc");
+	@SuppressWarnings("unchecked")
+	List<Film> resultList = query.getResultList();
+	return resultList;
     }
-    
+
     public void save(Film film) {
-    	manager.merge(film);
+	manager.merge(film);
     }
 
 }
